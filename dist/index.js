25b83501e19c955e56914e2f311d4ca5b9fc2160
deepak-parmar <dgparmar1999@rediffmail.com>
2022-03-22 04:59:01 +0530

    Updates

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import React from "../snowpack/pkg/react.js";
-import ReactDOM from "../snowpack/pkg/react-dom.js";
+import React from "../_snowpack/pkg/react.js";
+import ReactDOM from "../_snowpack/pkg/react-dom.js";
 import DrumMachine from "./DrumMachine.js";
 ReactDOM.render(/* @__PURE__ */ React.createElement(DrumMachine, null), document.getElementById("app"));
