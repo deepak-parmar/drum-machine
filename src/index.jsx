77e9c03fdac40ca387c0490142027133b6fee942
deepak-parmar <dgparmar1999@rediffmail.com>
2022-03-22 03:53:10 +0530

    Add index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import DrumMachine from "./DrumMachine"
+
+// ReactDOM.render(<DrumMachine />, document.getElementById("app"));
+ReactDOM.render(<h1>Coming Soon!</h1>, document.getElementById("app"));
