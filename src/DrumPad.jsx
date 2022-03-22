420df5691764ef672f826c31757ba25051621d7d
deepak-parmar <dgparmar1999@rediffmail.com>
2022-03-22 05:49:53 +0530

    Add eventListeners for .drum-pad active state

--- src/DrumPad.jsx
+++ src/DrumPad.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./DrumPad.css"
+
 
 class DrumPad extends React.Component {
   constructor(props) {
@@ -7,11 +7,13 @@ class DrumPad extends React.Component {
     this.handlePlay = this.handlePlay.bind(this);
   }
 
-  componentDidMount = () =>
-  document.addEventListener("keyup", (event) => this.handlePlay(event.keyCode));
-
+  componentDidMount = () => {
+    document.addEventListener("keydown", (event) => this.handlePlay(event.keyCode));
+    document.addEventListener("keyup", () => document.getElementById(this.props.label.toLowerCase()).classList.remove("drum-pad-active"));
+  }
+  
   componentWillUnmount = () =>
-  document.removeEventListener("keyup", (event) => this.handlePlay(event.keyCode));
+  document.removeEventListener("keydown", (event) => this.handlePlay(event.keyCode));
     
   handlePlay = (keyCode) => {
     if (keyCode == this.props.keyCode) {
@@ -19,6 +21,8 @@ class DrumPad extends React.Component {
       clip.curentTime = 0;
       clip.play();
       this.props.setDisplay(this.props.clip);
+      document.getElementById(this.props.label.toLowerCase()).classList.add("drum-pad-active");
+      setTimeout(() => document.getElementById(this.props.label.toLowerCase()).classList.remove("drum-pad-active"), 100)
     }
   }
 
